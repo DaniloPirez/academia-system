77b361cfaa411e1aec0b06964681560286608efd
Danilo Pires <danilopiressousa@gmail.com>
2026-03-15 01:55:47 -0300

    fix: ajusta CORS para localhost e API

--- academia-aluno/src/api/api.ts
+++ academia-aluno/src/api/api.ts
@@ -1,15 +1,18 @@
 import axios from "axios"
 
 export const api = axios.create({
-  baseURL:"https://academia-backend-5m3g.onrender.com"
+  baseURL: "https://academia-backend-5m3g.onrender.com",
+  headers: {
+    "Content-Type": "application/json"
+  }
 })
 
-api.interceptors.request.use(config=>{
+api.interceptors.request.use((config) => {
 
-  const token = localStorage.getItem("token")
+  const token = localStorage.getItem("cliente_token")
 
-  if(token){
-    config.headers.Authorization=`Bearer ${token}`
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`
   }
 
   return config
