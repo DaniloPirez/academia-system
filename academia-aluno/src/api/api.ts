b5ab3d9dc570d3953047a51ce56f03f98e611c91
Danilo Pires <danilopiressousa@gmail.com>
2026-03-15 02:00:00 -0300

    fix: ajusta CORS para VITE e API

--- academia-aluno/src/api/api.ts
+++ academia-aluno/src/api/api.ts
@@ -1,18 +1,15 @@
 import axios from "axios"
 
 export const api = axios.create({
-  baseURL: "https://academia-backend-5m3g.onrender.com",
-  headers: {
-    "Content-Type": "application/json"
-  }
+  baseURL:"https://academia-backend-5m3g.onrender.com"
 })
 
-api.interceptors.request.use((config) => {
+api.interceptors.request.use(config=>{
 
-  const token = localStorage.getItem("cliente_token")
+  const token = localStorage.getItem("token")
 
-  if (token) {
-    config.headers.Authorization = `Bearer ${token}`
+  if(token){
+    config.headers.Authorization=`Bearer ${token}`
   }
 
   return config
