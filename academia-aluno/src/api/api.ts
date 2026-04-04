b967f4da7570dc7a8d9458f40007697ed0232de7
Danilo Pires <danilopiressousa@gmail.com>
2026-04-04 20:07:31 -0300

    fix: ajuste de login

--- academia-aluno/src/api/api.ts
+++ academia-aluno/src/api/api.ts
@@ -1,12 +1,13 @@
-import axios from "axios"
+import axios from "axios";
 
 export const api = axios.create({
-  baseURL:"https://academia-backend-5m3g.onrender.com"
-})
+  baseURL: "https://academia-backend-5m3g.onrender.com",
+});
 
-api.interceptors.request.use(config => {
+api.interceptors.request.use((config) => {
   const token =
     localStorage.getItem("cliente_token") ||
+    localStorage.getItem("access_token") ||
     localStorage.getItem("token");
 
   if (token) {
@@ -15,3 +16,22 @@ api.interceptors.request.use(config => {
 
   return config;
 });
+
+api.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if (error.response?.status === 401) {
+      localStorage.removeItem("cliente_token");
+      localStorage.removeItem("access_token");
+      localStorage.removeItem("token");
+
+      const rotaAtual = window.location.pathname;
+
+      if (rotaAtual !== "/login") {
+        window.location.href = "/login";
+      }
+    }
+
+    return Promise.reject(error);
+  }
+);
